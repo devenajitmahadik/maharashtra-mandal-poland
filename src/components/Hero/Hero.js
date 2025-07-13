7d25bec097c36531001ce33cff2b6a0a31ad8459
Mahadik <dmahadik@luxoft.com>
2025-07-13 20:10:23 +0200

    Updates Hero section to display background image and welcome text

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,14 +1,16 @@
-import { Swiper, SwiperSlide } from "swiper/swiper-react.mjs";
-import { Autoplay } from "swiper/modules";
-import { heroImagesList } from "../../constants/hero";
-import { textToId } from "../../utils/helper";
-import "swiper/swiper.min.css";
+// import { Swiper, SwiperSlide } from "swiper/swiper-react.mjs";
+// import { Autoplay } from "swiper/modules";
+// import { heroImagesList } from "../../constants/hero";
+// import { textToId } from "../../utils/helper";
+// import "swiper/swiper.min.css";
 import "./hero.scss";
 
 const Hero = () => {
     return (
         <div className="hero-section">
-            <Swiper
+            <div className="hero-section-image-filter"></div>
+            <div className="hero-heading">Welcome to Maharashtra Mandal Poland</div>
+            {/* <Swiper
                 modules={[Autoplay]}
                 autoplay={{delay: 3000}} loop={true}>
                     {heroImagesList.map((listItem) => {
@@ -16,7 +18,7 @@ const Hero = () => {
                             <img src={listItem.src} alt={listItem.alt} />
                         </SwiperSlide>
                     })}
-            </Swiper>
+            </Swiper> */}
         </div>
     )
 };
